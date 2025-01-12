3d469fa975559077e1c2dbfa1356a86738b4c137
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2025-01-12 23:38:34 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection function
+const connectToMongoDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit the process if MongoDB connection fails
+    }
+};
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+    connectToMongoDB(); // Connect to MongoDB when the server starts
+    console.log(`Server Running on port ${PORT}`);
+});
